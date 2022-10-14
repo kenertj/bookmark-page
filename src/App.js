92dd3f29281a9819ccd52503282d48fd700fbc36
kenertj <tsoojkk@gmail.com>
2022-10-14 14:02:53 +0300

    boilerplate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Box } from '@mui/system'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Footer from './components/Footer';
+import Features from './components/Features';
+import Faq from './components/Faq';
+import Download from './components/Download';
+import Contact from './components/Contact';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Raleway, Open Sans, Roboto, sans-serif"
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Navbar />
+      <Hero />
+      <Features />
+      <Download />
+      <Faq />
+      <Contact />
+      <Footer />
+    </ThemeProvider>
   );
 }
 
